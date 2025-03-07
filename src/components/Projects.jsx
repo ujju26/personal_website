8bca86b2383458f99eb745614f560f099aef4310
ujju26 <ujjawalpatel2777@gmail.com>
2025-03-07 16:16:16 +0530

    Fixed project description not showing on mobile and improved hover effect.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -30,9 +30,9 @@ const Projects = () => {
                           initial={{ opacity: 0 }}
                           whileHover={{ opacity: 1 }}
                           transition={{ duration: 0.5 }}
-                          className="absolute inset-0 flex flex-col items-center justify-center text-white opacity-0 backdrop-blur-lg transition-opacity duration-500 group-hover:opacity-100">
+                          className="absolute inset-0 flex flex-col items-center justify-center text-white opacity-0 backdrop-blur-lg transition-opacity duration-500 group-hover:opacity-100 active:opacity-100">
                            <h3 className="mb-2 text-xl">{project.name}</h3>
-                           <p className="mb-12 p-4">{project.description}</p>
+                           <p className="mb-12 p-4 text-center">{project.description}</p>
                            <a href={project.githubLink} target="_blank" rel="noopener noreferrer" className="rounded-full bg-white px-4 py-2 text-black hover:bg-gray-300">
                                <div className="flex items-center">
                                    <span>View on GitHub</span>
